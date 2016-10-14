b5245845159a7c0a6e5dc244552aa480df887a1d
Troels Blicher Petersen <troels@newtec.dk>
2016-10-14 14:58:22 +0200

    Fixed randomgen.sh
    *Nu virker randomgen.sh med en parameter der bestemmer antallet af tal i test.dat

--- randomgen.sh
+++ randomgen.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 #Creates a test file, with a user specified amount of random numbers.
-echo > test.dat
-for i in {1..$1}; do echo $RANDOM; done >> test.dat
+max=$(($1+0))
+echo $max
+for((i=1;i<=$1;i++)) 
+	do echo $RANDOM 
+	done > test.dat
