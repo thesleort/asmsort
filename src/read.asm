a9503bc56e2027afac35982c1d815c0de39c6d52
Troels Blicher Petersen <troels@newtec.dk>
2016-10-18 17:15:55 +0200

    read.asm now works
    *Formatted read.asm to become more readable

--- src/read.asm
+++ src/read.asm
@@ -1,40 +1,49 @@
 .section .data
-buffer: .space 65536
+	buffer: .space 2048
 
 newline: .string "\n"
+
+.section .bss
+	.lcomm file_buffer, 2048
+
+.set SYS_READ, 	0
+.set SYS_WRITE, 1
+.set SYS_OPEN, 	2
+.set SYS_EXIT,	60
+.set STDOUT, 	1
+
+
 
 .section .text
 .globl _start
 _start:
+	mov 16(%RSP), 		%R8			# File to open
 
-	mov 16(%rsp), %r8	#File to open
+_open:
+	mov $SYS_OPEN, 		%RAX		# Open file
+	mov %R8, 			%RDI		# File descriptor
+	mov $0, 			%RSI		# Interrupt flag
+	syscall
+	mov %RAX, 			%R9
+	
+_read:
+	mov $SYS_READ, 		%RAX		# Read file
+	mov %R9, 			%RDI		# File descriptor from before
+	mov $file_buffer, 	%RSI		# 
+	mov $5, 			%RDX		# Read one char at a time
+	syscall
+	cmp $0, 			%RAX		# Ensure haven't read EOF
+	je _exit						# Stop reading file
 
-	mov $2, %rax	#Open file
-	mov %r8, %rdi
-	mov $0, %rsi
-	syscall
-	
-	mov %rax, %r9		
+_write:								# Put on stack/list instead of printing it.
+	mov %RAX, 			%RDX		# Print whatever is in RAX
+	mov $SYS_WRITE,		%RAX		# Write
+	mov $STDOUT, 		%RDI		# Standard output
+	mov $file_buffer, 	%RSI
+	syscall							
+	jmp _read						# Read file again
 
-	mov $0, %rax	#Read file
-	mov %r9, %rdi
-	mov $buffer, %rsi
-	mov $11, %rdx
-	syscall
-	
-	mov $1, %rax	#Write to command line
-	mov $1, %rdi
-	mov $buffer, %rsi
-	mov $7, %rdx
-	syscall
-	
-	
-	mov $1, %rax	#Write a new line
-	mov $1, %rdi
-	mov $newline, %rsi
-	mov $2, %rdx
-	syscall
-
-	mov $60, %rax
-	mov $0, %rdi
+_exit:
+	mov $SYS_EXIT, 		%RAX		# Terminate program
+	mov $0, 			%RDI
 	syscall
